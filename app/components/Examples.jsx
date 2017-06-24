81f3a7911213b47236ace67997b9fb373fdbde50
juicydrey <my.sabaka@gmail.com>
2017-06-24 14:54:22 -0400

    Centered text, added content to examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out: </p>
+      <ol>
+        <li>
+          <Link to='/?location=Charlotte'>Charlotte, NC</Link>
+        </li>
+        <li>
+          <Link to='/?location=London'>London, United Kingdom</Link>
+        </li>
+        <li>
+          <Link to='/?location=Toronto'>Toronto, Canada</Link>
+        </li>
+      </ol>
     </div>
   )
 };

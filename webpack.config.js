471e25d9342810b1e6a47019681cc32f024213d7
juicydrey <my.sabaka@gmail.com>
2017-06-24 21:38:23 -0400

    Added error modal

--- webpack.config.js
+++ webpack.config.js
@@ -25,11 +25,12 @@ module.exports = {
       Main: 'app/components/Main.jsx',
       Nav: 'app/components/Nav.jsx',
       Weather: 'app/components/Weather.jsx',
-      WeatherForm: 'app/components/WeatherForm',
-      WeatherMessage: 'app/components/WeatherMessage',
+      WeatherForm: 'app/components/WeatherForm.jsx',
+      WeatherMessage: 'app/components/WeatherMessage.jsx',
       About: 'app/components/About.jsx',
       Examples: 'app/components/Examples.jsx',
-      openWeatherMap: 'app/api/openWeatherMap.jsx'
+      openWeatherMap: 'app/api/openWeatherMap.jsx',
+      ErrorModal: 'app/components/ErrorModal.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
